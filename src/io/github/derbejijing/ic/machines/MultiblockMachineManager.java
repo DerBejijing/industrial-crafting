c903a7376cac62892c3d745b32dbfe2f2357c3fe
DerBejijing <verunsicherungsgmbhundco@gmail.com>
2023-03-22 19:17:38 +0100

    outsource multiblock registry

--- src/io/github/derbejijing/ic/machines/MultiblockMachineManager.java
+++ src/io/github/derbejijing/ic/machines/MultiblockMachineManager.java
@@ -5,40 +5,11 @@ import java.util.ArrayList;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-import io.github.derbejijing.ic.machines.multiblock.ChemistryWorkbench;
+import io.github.derbejijing.ic.machines.MultiblockRegistry.RegistryEnum;
 
 public class MultiblockMachineManager {
     private ArrayList<MultiblockMachine> machines;
 
-    private interface MultiblockRegistry<T extends MultiblockMachine> {
-        int get_id();
-        Class<? extends MultiblockMachine> get_class();
-    }
-
-    public enum MultiblockRegistryEnum implements MultiblockRegistry<MultiblockMachine> {
-        CHEMISTRY_WORKBENCH(0, ChemistryWorkbench.class);
-
-        public final int id;
-        public final Class<? extends MultiblockMachine> machine_class;
-
-        private MultiblockRegistryEnum(int id, Class<? extends MultiblockMachine> machine_class) {
-            this.id = id;
-            this.machine_class = machine_class;
-        }
-
-        public int get_id() {
-            return id;
-        }
-
-        public Class<? extends MultiblockMachine> get_class() {
-            return machine_class;
-        }
-
-        public static Class<? extends MultiblockMachine> get_by_id(int id) {
-            for(MultiblockRegistryEnum mr : MultiblockRegistryEnum.values()) if(id == mr.get_id()) return mr.get_class();
-            return null;
-        }
-    }
 
     public MultiblockMachineManager() {
 
@@ -46,7 +17,7 @@ public class MultiblockMachineManager {
 
 
     public void place(Player player, Location location, int orientation, int id) {
-        MultiblockMachine.place(location, orientation, MultiblockRegistryEnum.get_by_id(id));
+        MultiblockMachine.place(location, orientation, RegistryEnum.get_by_id(id));
     }
 
 
---
+++ src/io/github/derbejijing/ic/machines/MultiblockRegistry.java
@@ -0,0 +1,35 @@
+package io.github.derbejijing.ic.machines;
+
+import io.github.derbejijing.ic.machines.multiblock.ChemistryWorkbench;
+
+public class MultiblockRegistry {
+    private interface Registry<T extends MultiblockMachine> {
+        int get_id();
+        Class<? extends MultiblockMachine> get_class();
+    }
+
+    public enum RegistryEnum implements Registry<MultiblockMachine> {
+        CHEMISTRY_WORKBENCH(0, ChemistryWorkbench.class);
+
+        public final int id;
+        public final Class<? extends MultiblockMachine> machine_class;
+
+        private RegistryEnum(int id, Class<? extends MultiblockMachine> machine_class) {
+            this.id = id;
+            this.machine_class = machine_class;
+        }
+
+        public int get_id() {
+            return id;
+        }
+
+        public Class<? extends MultiblockMachine> get_class() {
+            return machine_class;
+        }
+
+        public static Class<? extends MultiblockMachine> get_by_id(int id) {
+            for(RegistryEnum mr : RegistryEnum.values()) if(id == mr.get_id()) return mr.get_class();
+            return null;
+        }
+    }
+}
