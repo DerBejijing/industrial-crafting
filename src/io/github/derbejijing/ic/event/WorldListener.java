d701a0d7ec9c7076bd0104c93a0193feb8b7f110
DerBejijing <verunsicherungsgmbhundco@gmail.com>
2023-04-10 20:01:35 +0200

    make flammable chemicals explode

--- src/io/github/derbejijing/ic/event/WorldListener.java
+++ src/io/github/derbejijing/ic/event/WorldListener.java
@@ -5,10 +5,13 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
@@ -20,6 +23,13 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 
 import io.github.derbejijing.ic.Main;
+import io.github.derbejijing.ic.chemical.Chemical;
+import io.github.derbejijing.ic.chemical.material.Acetone;
+import io.github.derbejijing.ic.chemical.material.AcetonePeroxide;
+import io.github.derbejijing.ic.chemical.material.Ethanol;
+import io.github.derbejijing.ic.chemical.material.GunPowder;
+import io.github.derbejijing.ic.chemical.material.Methanol;
+import io.github.derbejijing.ic.chemical.material.PrimerPowder;
 import io.github.derbejijing.ic.machines.component.InterfaceUtils;
 
 public class WorldListener implements Listener {
@@ -69,6 +79,48 @@ public class WorldListener implements Listener {
                         inventory.remove(item_stack);
                     }
                 }
+            }
+        }
+    }
+
+
+    @EventHandler
+    public void item_burn_event(EntityCombustEvent e) {
+        if(e.getEntityType().equals(EntityType.DROPPED_ITEM)) {
+            Entity entity = e.getEntity();
+            ItemStack item = ((Item)entity).getItemStack();
+
+            Chemical chemical = Chemical.get_from(item);
+            if(chemical == null) return;
+
+            if(chemical.getClass().equals(AcetonePeroxide.class)) {
+                entity.remove();
+                entity.getWorld().createExplosion(entity.getLocation(), 3, true);
+            }
+
+            if(chemical.getClass().equals(Acetone.class)) {
+                entity.remove();
+                entity.getWorld().createExplosion(entity.getLocation(), 1, true);
+            }
+
+            if(chemical.getClass().equals(Methanol.class)) {
+                entity.remove();
+                entity.getWorld().createExplosion(entity.getLocation(), 1, true);
+            }
+
+            if(chemical.getClass().equals(Ethanol.class)) {
+                entity.remove();
+                entity.getWorld().createExplosion(entity.getLocation(), 1, true);
+            }
+
+            if(chemical.getClass().equals(GunPowder.class)) {
+                entity.remove();
+                entity.getWorld().createExplosion(entity.getLocation(), 2, true);
+            }
+
+            if(chemical.getClass().equals(PrimerPowder.class)) {
+                entity.remove();
+                entity.getWorld().createExplosion(entity.getLocation(), 1, true);
             }
         }
     }
