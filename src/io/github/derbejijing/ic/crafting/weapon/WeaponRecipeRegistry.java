7bfa2cdf928dbc154a9a07654ff5a1f977c96eb9
DerBejijing <verunsicherungsgmbhundco@gmail.com>
2023-04-10 16:36:26 +0200

    added pistols

--- src/io/github/derbejijing/ic/crafting/weapon/WeaponRecipeRegistry.java
+++ src/io/github/derbejijing/ic/crafting/weapon/WeaponRecipeRegistry.java
@@ -1,12 +1,18 @@
 package io.github.derbejijing.ic.crafting.weapon;
 
+import io.github.derbejijing.ic.crafting.weapon.recipe.Weapon1911;
 import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponAk47;
 import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponAugA3;
+import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponDesertEagle;
 import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponFamas;
 import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponFnFal;
 import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponG3A3;
+import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponGlock17;
+import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponGlock18;
 import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponM16A4;
 import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponM4A1;
+import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponM9;
+import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponPM;
 import io.github.derbejijing.ic.crafting.weapon.recipe.WeaponScarH;
 
 public enum WeaponRecipeRegistry {
@@ -17,7 +23,13 @@ public enum WeaponRecipeRegistry {
     M4A1(4, "M4A1", WeaponM4A1.class),
     G3A3(5, "G3A3", WeaponG3A3.class),
     FAMAS(6, "Famas", WeaponFamas.class),
-    SCAR_H(7, "Scar H", WeaponScarH.class);
+    SCAR_H(7, "Scar H", WeaponScarH.class),
+    P1911(8, "1911", Weapon1911.class),
+    DESERT_EAGLE(9, "Desert Eagle", WeaponDesertEagle.class),
+    GLOCK_17(10, "Glock 17", WeaponGlock17.class),
+    GLOCK_18(11, "Glock 18", WeaponGlock18.class),
+    M9(12, "M9", WeaponM9.class),
+    PM(13, "PM", WeaponPM.class);
 
     public final int id;
     public final String name;
