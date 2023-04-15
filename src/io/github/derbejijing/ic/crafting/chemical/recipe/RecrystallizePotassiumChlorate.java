87577054296faead14fe42e559976eb6276cea0b
Der Bejijing <62348565+DerBejijing@users.noreply.github.com>
2023-04-15 16:01:52 +0200

    Create RecrystallizePotassiumChlorate.java

---
+++ src/io/github/derbejijing/ic/crafting/chemical/recipe/RecrystallizePotassiumChlorate.java
@@ -0,0 +1,31 @@
+package io.github.derbejijing.ic.crafting.chemical.recipe;
+
+import io.github.derbejijing.ic.chemical.ChemicalItem;
+import io.github.derbejijing.ic.chemical.property.ChemicalPurity;
+import io.github.derbejijing.ic.crafting.chemical.ChemicalRecipe;
+import io.github.derbejijing.ic.machines.MultiblockMachine;
+
+public class RecrystallizePotassiumChlorate extends ChemicalRecipe {
+
+    public RecrystallizePotassiumChlorate(MultiblockMachine master) {
+        super(master, 10, 60, 0);
+    }
+
+
+    @Override
+    public void add_ingredients() {
+        this.add_ingredient(ChemicalItem.POTASSIUM_CHLORATE, 1);
+    }
+
+
+    @Override
+    public void add_requirements() {
+        this.add_requirement(ChemicalItem.DISTILLED_WATER, 1);
+    }
+
+
+    @Override
+    public void add_outputs() {
+        this.add_output(ChemicalItem.POTASSIUM_CHLORATE, 1, ChemicalPurity.INDUSTRIAL_GRADE);
+    }
+}
