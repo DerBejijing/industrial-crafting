fdeb3fe99930850f796cb945fa818b67a05c2deb
Der Bejijing <62348565+DerBejijing@users.noreply.github.com>
2023-04-15 19:22:50 +0200

    Update MultiblockRegistry.java

--- src/io/github/derbejijing/ic/machines/MultiblockRegistry.java
+++ src/io/github/derbejijing/ic/machines/MultiblockRegistry.java
@@ -8,6 +8,7 @@ import io.github.derbejijing.ic.machines.multiblock.Furnace;
 import io.github.derbejijing.ic.machines.multiblock.Macerator;
 import io.github.derbejijing.ic.machines.multiblock.Reactor;
 import io.github.derbejijing.ic.machines.multiblock.Recrystallizer;
+import io.github.derbejijing.ic.machines.multiblock.SolarCondenser;
 import io.github.derbejijing.ic.machines.multiblock.SolarElectrolyzer;
 import io.github.derbejijing.ic.machines.multiblock.WeaponAssembly;
 
@@ -24,10 +25,11 @@ public class MultiblockRegistry {
         CENTRIFUGE(4, Centrifuge.class),
         MACERATOR(5, Macerator.class),
         SOLAR_ELECTROLYZER(6, SolarElectrolyzer.class),
-        FURNACE(7, Furnace.class),
-        REACTOR(8, Reactor.class),
-        RECRYSTALLIZER(9, Recrystallizer.class),
-        WEAPON_ASSEMBLY(10, WeaponAssembly.class);
+        SOLAR_CONDENSER(7, SolarElectrolyzer.class),
+        FURNACE(8, Furnace.class),
+        REACTOR(9, Reactor.class),
+        RECRYSTALLIZER(10, Recrystallizer.class),
+        WEAPON_ASSEMBLY(11, WeaponAssembly.class);
 
         public final int id;
         public final Class<? extends MultiblockMachine> machine_class;
